f03c8d97ee4e2f59aea899b84949fabcfe051abf
raich uddin <rabbitrade10@gmail.com>
2021-08-30 23:23:44 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,10 @@
+const raich = document.getElementById('button-submit').
+    addEventListener('click', function () {
+        const userEmail = document.getElementById('user-email');
+        const userText = userEmail.value;
+        const userPassword = document.getElementById('user-password').value;
+        if (userText == 'rabbitrade10@gmail.com' && userPassword
+            == 'raich') {
+            window.location.href = 'banking.html';
+        }
+    });
